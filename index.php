b6fbc66e5efe1459c3bc2e8aa1eb82beaca1b7cc
Joachim Rives <47552793+SS-Runen@users.noreply.github.com>
2021-03-19 13:47:39 +0800

    Added all variants of PDO code to connect to MySQL server instances local or AWS.

--- index.php
+++ index.php
@@ -5,12 +5,24 @@ try {
     PHP class definition:
     public __construct ( string $dsn , string $username = ? , string $passwd = ? , array $options = ? )
     */
+    
+    // $pdo = new PDO(
+    //     $dsn = "127.0.0.1",
+    //     $username = "root",
+    //     $passwd = ""
+    // );
+
     $pdo = new PDO(
-        $dsn = "127.0.0.1",
+        $dsn = "mysql:host=127.0.0.1;dbname=wt_perfmon;",
         $username = "root",
-        $passwd = "root",
-        $options = []
+        $passwd = ""
     );
+
+    // $pdo = new PDO(
+    //     $dsn = "mysql:host=db-wtperfmon.chvhk2xcrgnq.ap-southeast-1.rds.amazonaws.com;dbname=wt_perfmon;",
+    //     $username = "admin",
+    //     $passwd = "vVYb9mvF"
+    // );
 } catch (PDOException $e) {    
     echo "<pre>"."Could not connect to database. Error:\n$e"."</pre>";
 }
